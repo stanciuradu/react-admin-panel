efdca53718b95121688ee5aab364400c5e0defc5
radu <radustanciu841@yahoo.ro>
2021-04-09 20:52:00 +0300

    Component UserItem

--- src/components/UserItem.jsx
+++ src/components/UserItem.jsx
@@ -1,17 +1,23 @@
 import React from 'react';
 
 function UserItem(props) {
-    const {name, email, isGoldClient} = props;
-
+    
+    const {name, email, isGoldClient, wage, src, button} = props;
+    
     return (
-        <div>
+        <div className='user-item'>
             <h3>{ name }</h3>
             <p>{ email }</p>
+            <p>{wage}</p>
+            <img src={src} alt=''/>
             { isGoldClient
                 ? <h3>Client GOLD</h3>
                 : null
             }
+            <button type={button}>Sterge userul</button>
+            
         </div>
+        
     );
 }
 
