0c765801744052a98e538b520be0c9f055a7b033
radu <radustanciu841@yahoo.ro>
2021-04-09 20:53:02 +0300

    Add wage,src in this component

--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -5,13 +5,15 @@ function UserList(props) {
     const { users } = props;
 
     return (
-        <div>
+        <div className='user-list'> 
             <h2>Lista utilizatorilor:</h2>
             { users.map((user, index) => {
                 return <UserItem
                     id={ user.id }
                     name={ user.name }
                     email={ user.email }
+                    wage={ user.wage }
+                    src={ user.src }
                     isGoldClient={ user.isGoldClient }
                     key={ index }
                 />
